fae0d4cf5c7a156bc42f7a6670d5098bc4ba1a35
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-09 19:13:46 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,7 +4,10 @@ def hello_t(array)
   yield array[i]
   i = i + 1
 end 
-array
+  array
+  else 
+    puts "Hey! No block was given!"
+  end
 end
   hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
